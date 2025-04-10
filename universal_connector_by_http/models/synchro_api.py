c906835fa37ab16fab70a9e2de648b5f1bc71988
zeroincombenze <info@shs-av.com>
2025-04-10 13:49:29 +0200

    [IMP] Mini-refactoring

--- universal_connector_by_http/models/synchro_api.py
+++ universal_connector_by_http/models/synchro_api.py
@@ -23,6 +23,7 @@ class SynchroApi(models.Model):
         session = self.init_session(
             login_endpoint=endpoint, data_endpoint=data_endpoint
         )
+        cnx = False
         try:
             if headers:
                 cnx = requests.get(endpoint, headers=headers, verify=verify)
